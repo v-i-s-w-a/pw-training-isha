44f0a09da62ac8db8c078a29afea7a3d415a7c6c
ishadevi <isha2k1210@gmail.com>
2026-08-11 11:51:50 +0530

    Add Session 3 Playwright project (#3)

---
+++ tests/isha.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+
+test('Check SauceDemo website', async ({ page }) => {
+
+await page.goto('https://www.saucedemo.com/');
+
+await expect(page).toHaveTitle('Swag Labs');
+
+await expect(page).toHaveURL('https://www.saucedemo.com/');
+
+});
